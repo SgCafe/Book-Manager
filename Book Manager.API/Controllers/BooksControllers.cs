7a126bf35bca0b8af4737f0dd6abb8308f87aa44
Sergio-Sum <105067158+SgCafe@users.noreply.github.com>
2024-08-03 08:07:18 -0300

    :sparkles: feat: finalizando estrutura do projeto

---
+++ Book Manager.API/Controllers/BooksControllers.cs
@@ -0,0 +1,34 @@
+﻿using Book_Manager.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Manager.API.Controllers;
+
+    [Route("api/books")]
+    [ApiController]
+public class BooksControllers : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult GetAll(string search)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Post(CreateBookInputModel model, int id)
+    {
+        return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return Ok();
+    }
+}
---
+++ Book Manager.API/Models/CreateBookInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Book_Manager.API.Models;
+
+public class CreateBookInputModel
+{
+    public int IdBook { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public int YearPublication { get; set; }
+}
